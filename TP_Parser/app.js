c5bdb6b2f22d865d6ecf34521fa23703c1d1b22d
manuelkucharuk <manuelkucharuk@gmail.com>
2015-10-08 16:52:56 -0300

    Sigo con express, con errores. No carga bootstrap

--- TP_Parser/app.js
+++ TP_Parser/app.js
@@ -1,60 +1,46 @@
+var http = require('http');
 var express = require('express');
+var engine = require('ejs-locals');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Cargamos las rutas de nuestra aplicacion
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+//  Inicializamos la aplicacion
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Configuramos las rutas cargadas previamente
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+
+// Error handlers
+
+//  En caso de que ocurra un error del tipo 404 - Not Found manejamos el error
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Recurso no encontrado');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+//  En caso de que ocurra un error, configuramos la respuesta
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
 });
 
+//  Creamos el servidor http
+app.set('port',3000);
 
-module.exports = app;
+//  Inicializamos el servidor en el puerto 3000
+var server=app.listen(app.get('port'), function() {
+  console.log('Escuchando el puerto ' + server.address().port);
+});
